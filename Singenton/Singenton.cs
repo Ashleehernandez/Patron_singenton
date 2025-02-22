e2fcc291ba8d932124d8c1db44cc44953205a906
ashle <ashle@DESKTOP-VVPHGA7>
2025-02-21 21:37:47 -0400

    Add project files.

---
+++ Singenton/Singenton.cs
@@ -0,0 +1,26 @@
+﻿namespace Singenton
+{
+    public class Singenton
+    {
+        private static Singenton intanciar = null;
+
+        private Singenton()
+        {
+            
+        }
+
+        public static Singenton GetSingenton()
+        {
+            if(intanciar == null)
+            {
+                return new Singenton();
+            }
+            else
+            {
+                return intanciar;   
+            }
+        }
+
+
+    }
+}
